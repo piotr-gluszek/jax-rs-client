b5a5c1e990bc766b7bf6d54317b4f5f01a142346
Piotr Głuszek <piogluszek@gmail.com>
2018-09-02 10:16:29 +0200

    Add data models

---
+++ src/main/java/pl/piotrgluszek/client/model/Post.java
@@ -0,0 +1,40 @@
+package pl.piotrgluszek.client.model;
+
+public class Post {
+    private Long userId;
+    private Long id;
+    private String title;
+    private String content;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
